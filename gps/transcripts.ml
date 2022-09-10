6e7fe237b5f2aa094662fb74add1b2c6763e12dc
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2022-09-10 21:10:10 +0200

    cleaning extra debug log

--- gps/transcripts.ml
+++ gps/transcripts.ml
@@ -2256,10 +2256,8 @@ let lmath ~year ~firstname ~lastname d state =
   List.exists
       (fun cours ->
 match cours.code_cours with | None -> false | Some code_gps ->
-    let _ = Format.printf "LMATHTEST %s %s %s %s @ " code_gps firstname lastname year in
     code_mandatory_course_DI_maths year code_gps
       &&
-      let _ = Format.printf "LMATH %s %s %s %s @ " code_gps firstname lastname year in
       match Remanent_state.get_cursus_exception
               ~firstname ~lastname ~year ~code_gps state
       with
@@ -2267,7 +2265,6 @@ match cours.code_cours with | None -> false | Some code_gps ->
       | _, Some x ->
         let level = x.Public_data.class_level in
         let acronym = x.Public_data.class_dpt in
-        let _ = Format.printf "LMATH %s %s @ " level acronym in
         level = "L" && acronym = "DMA"
         )    d.cours
 
