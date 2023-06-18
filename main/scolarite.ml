3ecece9810886b32b3b78a1818e1a9f758983ad9
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2023-06-18 21:39:45 +0200

    Enable PVs

--- main/scolarite.ml
+++ main/scolarite.ml
@@ -166,10 +166,10 @@ let state =
            match gps with
            | None -> state
            | Some gps ->
-             (*let report = true in*)
+             let report = true in
              let state, input =
                Transcripts.export_transcript
-                 ~output ~keep_success:true (*~report*) state gps
+                 ~output ~keep_success:true ~report state gps
              in
              let state =
                match input, is_dma || is_phys || is_chimie || is_geosciences
