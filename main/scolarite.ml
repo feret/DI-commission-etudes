f4d0d513a41061bd00cbc4b12d4c059dc4b43826
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2024-09-11 04:54:05 +0200

    eessai

--- main/scolarite.ml
+++ main/scolarite.ml
@@ -38,6 +38,8 @@ let state =
   Collect_pegasus_administrative_status.get_pegasus_administrative_data state
 let state =
   Collect_pegasus_pedagogical_registrations.get_pegasus_pedagogical_registrations state
+let state, t = Remanent_state.Collector_pegasus_stages.get state 
+let () = Pegasus_stages.dump t
 let state =
     let state, b = Remanent_state.do_we_log_pegasus_entries state in
     if b then
@@ -49,6 +51,8 @@ let state =
       let () = Pegasus_pedagogical_registrations.dump t in
       let state, t = Remanent_state.Collector_pegasus_notes.get state in
       let () = Pegasus_notes.dump t in
+      let state, t = Remanent_state.Collector_pegasus_stages.get state in
+      let () = Pegasus_stages.dump t in
       state
     else state
 let state =
