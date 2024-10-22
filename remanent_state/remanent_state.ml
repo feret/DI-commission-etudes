a928b66a3a41bc71c4dbd827cb3d92d925e78186
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2024-10-22 05:33:11 +0200

    IBENS

--- remanent_state/remanent_state.ml
+++ remanent_state/remanent_state.ml
@@ -1772,7 +1772,7 @@ let close_event_opt step_kind_opt t =
 
 let list_dpt =
   [
-    "DI";"di";"DMA";"dma";"PHYS";"Phys";"phys";"chimie";
+    "IBENS";"DI";"di";"DMA";"dma";"PHYS";"Phys";"phys";"chimie";
     "gsc";"geos";"geosciences";"géos";"géosciences" ]
 
 let get_cmd_options () =
