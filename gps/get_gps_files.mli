654f6af7dd2fdb39e4f1477431c1c9f71a1ce50c
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2024-10-22 12:24:09 +0200

    IBENS

--- gps/get_gps_files.mli
+++ gps/get_gps_files.mli
@@ -1,4 +1,4 @@
-type dpt = Maths | PE | DRI | PHYS | CHIMIE | GEOSCIENCES
+type dpt = Maths | PE | DRI | PHYS | CHIMIE | GEOSCIENCES | IBENS
 type access_type =
     GPS of dpt option | Backup | Preempt | Warn
 
