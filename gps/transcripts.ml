df03d7e356d1fc42bfb52a87d086d5515f3cd761
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2024-10-29 23:49:09 -0400

    more logs

--- gps/transcripts.ml
+++ gps/transcripts.ml
@@ -7345,6 +7345,12 @@ let add_pegasus_entries ~firstname ~lastname state gps_file =
             Remanent_state.get_grade_in_pegasus
               ~firstname ~lastname ~codehelisa:code ~year:course.Public_data.pe_year state
           in
+          let state =
+            Remanent_state.warn
+                __POS__
+                (Format.sprintf "%s %s %s %s %s" firstname lastname code year (match grade with None -> "Not_found" | Some _ -> "Found"))
+                Exit state 
+          in
           let state, note, validation =
               match grade with
               | None ->
