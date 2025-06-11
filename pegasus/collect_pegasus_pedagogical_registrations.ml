b5aaaa5a2e4bf48cd955b23e07948cf726cf0c26
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2025-06-11 21:37:52 +0200

    remove MPRI internship

--- pegasus/collect_pegasus_pedagogical_registrations.ml
+++ pegasus/collect_pegasus_pedagogical_registrations.ml
@@ -491,8 +491,8 @@ let get_pegasus_pedagogical_registrations
                                       let entry = {entry with diploma = Some "ANM1INF"} in 
                                       let entry, (bset, state) = 
                                       entry, (update_diploma "ANM1INF - Master in Computer science (First year)" entry (bset,state)) in 
-                                      let course = "UNINF2-135 - MPRI 2.00 stage de recherche M2 du MPRI" in  
-                                      let ects = "30." in 
+                                    (*  let course = "UNINF2-045 - Stage long M1 étranger pays non francophone & Long interns" in  
+                                      let ects = "30." in*) 
                                       entry,  update_course course ects entry bset state
                                 | ""::"ANM2INFPRI - Master in Computer science (Second year) - Algorithmic Science "::_ -> 
                                   let entry = {entry with diploma = Some "ANM2INFPRI"} in 
