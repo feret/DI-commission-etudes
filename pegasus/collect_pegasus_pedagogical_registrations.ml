ab4e1a8e3aa3b17f602b21ceced6109a50606587
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2025-07-02 09:51:56 +0200

    MPRI2

--- pegasus/collect_pegasus_pedagogical_registrations.ml
+++ pegasus/collect_pegasus_pedagogical_registrations.ml
@@ -504,7 +504,11 @@ let get_pegasus_pedagogical_registrations
                                   let entry = {entry with diploma = Some "ANECHINTER"} in 
                                   (*  let entry, (bset, state) = *)
                                      entry, (update_diploma "ANECHINTER - International Exchange" entry (bset,state))
-                                | ""::"ANM1INF - Master in Computer science (First year)"::_ -> 
+                                | ""::"ANM2INFPRI - Master in Computer science (Second year) - Algorithmic Science"::_ -> 
+                                  let entry = {entry with diploma = Some "ANM2INFMPRI"} in 
+                                  (*  let entry, (bset, state) = *)
+                                     entry, (update_diploma "ANM2INFPRI - Master in Computer science (Second year) - Algorithmic Science" entry (bset,state))
+                                     | ""::"ANM1INF - Master in Computer science (First year)"::_ -> 
                                       let entry = {entry with diploma = Some "ANM1INF"} in 
                                    (*  let entry, (bset, state) = *)
                                       entry, (update_diploma "ANM1INF - Master in Computer science (First year)" entry (bset,state)) (*in*) 
