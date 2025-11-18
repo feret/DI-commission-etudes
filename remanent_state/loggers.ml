377c6d6ce2642fb94710a8f1ba749b708c4f97ce
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2025-11-18 09:41:17 +0100

    correct GPA in english too

--- remanent_state/loggers.ml
+++ remanent_state/loggers.ml
@@ -971,12 +971,12 @@ let () = fprintf logger
      {\\setcounter{vectsb}{\\fpeval{\\resects*\\factor}}}%%\n\
       {\\setcounter{vectsb}{0}}%%\n\
        %%\n\
-    \\addtocounter{total}{\\fpeval{\\thecects*\\thecnote}}%%\n\
     \\addtocounter{ects}{\\fpeval{\\thecects*\\factor}}%%\n\
     %%\n\
     \\addtocounter{vsnects}{\\fpeval{\\thevectsc*\\factor+\\thevectsa*\\factor+\\thevectsb*\\factor}}%%\n\
      %%\n\       #2 & #3 &  \\IfStrEq{#4}{%s}{sans note}{\\IfStrEq{#4}{%s}{not graded}{\\IfStrEq{#1}{compensation}{\\cellcolor{lightpink}{\\mynumprint{#4}}}{\\IfStrEq{#1}{unvalidated}{\\cellcolor{grey}{\\mynumprint{#4}}}{\\mynumprint{#4}}}}} & \\lettergrade{#4} & {\\pga{#4}} \\cr%%\n\
-    }%%\n\
+     \\addtocounter{total}{\\fpeval{\\thecects*\\thecnote}}%%\n\
+     }%%\n\
     %%\n\ " Tools.valide_sans_note Tools.valide_sans_note_en
 Tools.valide_sans_note Tools.valide_sans_note_en
         in
