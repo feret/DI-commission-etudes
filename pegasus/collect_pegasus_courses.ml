46b22a98787791833fea6e994f0c9f05aa373930
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2026-01-20 13:24:17 +0100

    more logs

--- pegasus/collect_pegasus_courses.ml
+++ pegasus/collect_pegasus_courses.ml
@@ -429,6 +429,8 @@ let make_dictionary state =
       Pegasus_courses.fold (fun course (state,acc) -> 
         let lib = course.Public_data.pegasus_libelle in 
         let lib' = Tools.hash_libelle2 lib in 
+        let state = Remanent_state.warn __POS__ (Format.sprintf "%s-%s (%s) %s %s" lib lib' course.Public_data.pegasus_year 
+        (Tools.unsome_string course.Public_data.pegasus_codegps) course.Public_data.pegasus_helisa) Exit state in 
         state, Public_data.StringMap.add lib' lib acc)
       map (state, Public_data.StringMap.empty)
   in 
