f9c5543e570d5a1ee66ac3f6664129a7aa916797
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2026-03-11 18:10:36 +0100

    focus on DUVAL

--- remanent_state/remanent_state.ml
+++ remanent_state/remanent_state.ml
@@ -318,7 +318,7 @@ let parameters =
     simple_m1 = 
     [Public_data.DMA,Reglements_pedagogiques.m1_maths]; 
     suggest_course_dispatching=false; 
-    focus_lastname = []; 
+    focus_lastname = ["DUVAL"]; 
     focus_firstname = []; 
   } 
 
