c500b9cedfc08bed50b1a7a453b6f8e7b238af76
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2026-07-12 21:11:15 +0200

    more logs

--- gps/transcripts.ml
+++ gps/transcripts.ml
@@ -6226,11 +6226,15 @@ let program
         in  
         let state = if is_focus then 
             Remanent_state.warn __POS__ 
-              (Format.sprintf "COURS %s %s %s %s %f %s %s" lastname codecours 
+              (Format.sprintf "COURS %s %s %s %s %f %s %s %s %s" lastname codecours 
               (Tools.unsome_string cours.code_cours_gps ) 
               (Tools.unsome_string cours.code_cours_helisa) 
               (match cours.ects with None -> 0. | Some f -> f) (Tools.unsome_string string)
-              (Public_data.string_of_dpt dpt))
+              (Public_data.string_of_dpt dpt)
+              (Tools.unsome_string cours.diplome)
+              ( Public_data.string_of_dpt_opt cours.diplome_dpt)
+              )
+
               Exit state
         else state 
 in 
@@ -6582,8 +6586,9 @@ in
         in
           let state = 
     if focus then 
-      Remanent_state.warn __POS__ (Format.sprintf "ADD EXTRA COURSE1 %s %s %f %f %s %s " lastname (Tools.unsome_string libelle)  (match ects with None -> 0. | Some a -> a) (match cours.ects with None -> 0. | Some a -> a) (Tools.unsome_string string)
-              (Public_data.string_of_dpt dpt)) Exit state 
+      Remanent_state.warn __POS__ (Format.sprintf "ADD EXTRA COURSE1 %s %s %f %f %s %s %s %s" lastname (Tools.unsome_string libelle)  (match ects with None -> 0. | Some a -> a) (match cours.ects with None -> 0. | Some a -> a) (Tools.unsome_string string)
+              (Public_data.string_of_dpt dpt)  (Tools.unsome_string cours.diplome)
+              ( Public_data.string_of_dpt_opt cours.diplome_dpt)) Exit state 
     else state 
   in   
         let cours = 
@@ -6597,8 +6602,9 @@ in
         let ects = cours.ects in 
        let state = 
     if focus then 
-      Remanent_state.warn __POS__ (Format.sprintf "ADD EXTRA COURSE2 %s %s %f %f %s %s " lastname (Tools.unsome_string libelle)  (match ects with None -> 0. | Some a -> a) (match cours.ects with None -> 0. | Some a -> a)  (Tools.unsome_string string)
-              (Public_data.string_of_dpt dpt)) Exit state 
+      Remanent_state.warn __POS__ (Format.sprintf "ADD EXTRA COURSE2 %s %s %f %f %s %s %s %s" lastname (Tools.unsome_string libelle)  (match ects with None -> 0. | Some a -> a) (match cours.ects with None -> 0. | Some a -> a)  (Tools.unsome_string string)
+              (Public_data.string_of_dpt dpt) (Tools.unsome_string cours.diplome)
+              ( Public_data.string_of_dpt_opt cours.diplome_dpt) ) Exit state 
     else state 
   in   
         let () =
@@ -6768,12 +6774,13 @@ in
         in  
         let state = if is_focus then 
             Remanent_state.warn __POS__ 
-               (Format.sprintf "COURSB %s %s %s %s %s %s %s" lastname codecours 
+               (Format.sprintf "COURSB %s %s %s %s %s %s %s %s %s" lastname codecours 
               (Tools.unsome_string cours.code_cours_gps ) 
               (Tools.unsome_string cours.code_cours_helisa) 
               (Tools.unsome_string cours.cours_annee)
               (Tools.unsome_string string)
-              (Public_data.string_of_dpt dpt))
+              (Public_data.string_of_dpt dpt)  (Tools.unsome_string cours.diplome)
+              ( Public_data.string_of_dpt_opt cours.diplome_dpt))
               Exit state
         else state 
 in
