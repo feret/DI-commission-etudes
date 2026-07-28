949b5cbd308958bbc2dcb57781fc1bd7dc0f21d1
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2026-07-28 08:05:47 +0100

    more logs

--- pegasus/collect_pegasus_courses.ml
+++ pegasus/collect_pegasus_courses.ml
@@ -466,6 +466,7 @@ let all_fields =
             let state = Remanent_state.open_event_opt event state in
             let state = Scan_xlss_files.collect_gen
               ~strict:false
+              ~debug:true 
               ?repository
               ?prefix
               ?file_name
