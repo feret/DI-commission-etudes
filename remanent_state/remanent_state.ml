c6f1094593d3e07da3f4c282dbd6f2a526d8489d
Jérôme FERET <jerome.feret@info.ens.psl.eu>
2026-09-21 22:11:37 +0200

    focus

--- remanent_state/remanent_state.ml
+++ remanent_state/remanent_state.ml
@@ -326,7 +326,7 @@ let parameters =
     simple_m1 = 
     [Public_data.DMA,Reglements_pedagogiques.m1_maths]; 
     suggest_course_dispatching=false; 
-    focus_lastname = ["MARQUET"]; 
+    focus_lastname = ["UTTERSTROM"]; 
     focus_firstname = []; 
     keep_not_validated = true ; 
     do_not_move_unvalidated = true ; 
